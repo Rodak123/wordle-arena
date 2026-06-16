33896dd88b6bf1613132f5dbb5a9a7fd4422b5bb
Rodak Titěra <50217333+Rodak123@users.noreply.github.com>
2026-06-17 01:31:26 +0200

    add: Added benchmark to dailyCron

--- src/dailyCron.ts
+++ src/dailyCron.ts
@@ -1,11 +1,14 @@
 import cron from 'node-cron';
 import { daily } from './daily.ts';
+import { benchmark } from './benchmark.ts';
 
 /**
  * Schedules daily wordle every day
  */
 export const dailyCron = () => {
   daily(false);
+  benchmark();
+
   cron.schedule(
     '0 0 6 * * *', // 06:00
     () => {
