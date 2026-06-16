919e0b4f480ee2f791d270010c3b95cce669cf67
Jiří Vrba <jiri@vrba.dev>
2026-06-16 20:44:47 +0200

    Add benchmarking + optimized table lookup bot

---
+++ src/benchmark.ts
@@ -0,0 +1,70 @@
+import { createWordle } from "./actions/createWordle.ts";
+import { runBotsAndSortResults } from "./actions/runBotsAndSortResults.ts";
+
+type BotResultsAggregation = {
+  solvedChallenges: number;
+  failedChallenges: number;
+  averageGuesses: number;
+  averageSolvingTimeMs: number;
+  wordsCausingCrash: string[];
+};
+
+/**
+ * Uses random word from the valid word list
+ */
+export const benchmark = async () => {
+  // load wordle
+  const wordle = createWordle();
+  const results: Record<string, BotResultsAggregation> = {};
+
+  for (const word of wordle.validWords) {
+    console.log(`Trying ${word}`);
+
+    wordle.loadExact(word);
+
+    const botResults = await runBotsAndSortResults(wordle);
+
+    botResults.forEach((result) => {
+      const name = result.meta.name;
+
+      results[name] ??= {
+        solvedChallenges: 0,
+        failedChallenges: 0,
+        averageGuesses: 0,
+        averageSolvingTimeMs: 0,
+        wordsCausingCrash: [],
+      };
+
+      const totalGuesses =
+        results[name].solvedChallenges + results[name].failedChallenges + 1;
+
+      switch (result.status) {
+        case "solved":
+          results[name].solvedChallenges++;
+          break;
+
+        case "failed":
+          results[name].failedChallenges++;
+          break;
+
+        case "crashed":
+          results[name].failedChallenges++;
+          results[name].wordsCausingCrash.push(word);
+          break;
+      }
+
+      results[name].averageSolvingTimeMs +=
+        result.solvingTimeMs / totalGuesses -
+        results[name].averageSolvingTimeMs / Math.max(totalGuesses - 1, 1);
+      results[name].averageGuesses +=
+        result.guesses.length / totalGuesses -
+        results[name].averageGuesses / Math.max(totalGuesses - 1, 1);
+    });
+  }
+
+  console.log(results);
+};
+
+if (import.meta.main) {
+  benchmark();
+}
